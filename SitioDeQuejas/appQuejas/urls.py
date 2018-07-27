948098ca59ad5db85a3ede2c6d7024570a204c89
liarias <lucio_arias96@hotmail.com>
2018-07-27 17:47:21 -0500

    plantilla y informacion estatica cargada (probar :v)

--- SitioDeQuejas/appQuejas/urls.py
+++ SitioDeQuejas/appQuejas/urls.py
@@ -3,5 +3,10 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path(r'', views.cargarNoticias),
+    path(r'', views.cargarNoticias,name='index'),
+    path(r'quienesSomos/',views.quienesSomos,name='quienesSomos'),
+    path(r'categorias/',views.categorias,name='categorias'),
+    path(r'registro/',views.registro,name='registro'),
+    path(r'iniciarSesion/',views.iniciarSesion,name='iniciarSesion'),
+    path(r'contactenos/',views.contactenos,name='contactenos'),
 ]
