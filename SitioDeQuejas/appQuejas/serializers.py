3a54748496eb34a9e0d962f7e66a0c5e0a2c6440
AlexUbillaTorres <33100671+AlexUbillaTorres@users.noreply.github.com>
2018-08-19 21:49:01 -0500

    Update serializers.py

--- SitioDeQuejas/appQuejas/serializers.py
+++ SitioDeQuejas/appQuejas/serializers.py
@@ -2,17 +2,20 @@ from rest_framework import serializers
 from .models import *
 
 
+
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = ('id', 'nombre')
 
+	
 class PersonaSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     class Meta:
         model = Persona
         fields = ('id', 'user', 'nombrePersona', 'apellidoPersona','profesion', 'numeroContacto')
 
+	
 class QuejaSerializer(serializers.ModelSerializer):
 	user = serializers.ReadOnlyField(source='user.username')
 	categoria = serializers.ReadOnlyField(source='categoria.nombre')
@@ -20,6 +23,7 @@ class QuejaSerializer(serializers.ModelSerializer):
 		model = Queja
 		fields = ('id', 'titulo', 'categoria', 'fechaCreacion', 'imagen', 'descripcion','user')
 
+		
 class ComentarioSerializer(serializers.ModelSerializer):
     usuario = serializers.ReadOnlyField(source='user.username')
     queja = serializers.ReadOnlyField(source='queja.titulo')
