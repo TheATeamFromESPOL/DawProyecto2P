88a05b9c1fb1f74bc95242a66c58dc405026875b
Karol <knle21597@gmail.com>
2018-08-25 19:05:01 -0500

    Mongo

---
+++ SitioDeQuejas/reportes/routers.py
@@ -0,0 +1,39 @@
+class MyApp2Router(object):
+    """
+    A router to control all database operations on models in
+    the myapp2 application
+    """
+ 
+    def db_for_read(self, model, **hints):
+        """
+        Point all operations on myapp2 models to 'my_db_2'
+        """
+        if model._meta.app_label == 'reportes':
+            return 'dbreportes'
+        return None
+ 
+    def db_for_write(self, model, **hints):
+        """
+        Point all operations on myapp models to 'other'
+        """
+        if model._meta.app_label == 'reportes':
+            return 'dbreportes'
+        return None
+ 
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Allow relations if a model in the auth app is involved.
+        """
+        if obj1._meta.app_label == 'reportes' or \
+           obj2._meta.app_label == 'reportes':
+           return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """
+        Make sure the auth app only appears in the 'auth_db'
+        database.
+        """
+        if app_label == 'reportes':
+            return db == 'dbreportes'
+        return None
